982508c9e100b6186fa1a52e89ca7091f493548d
winthanhtike147 <mmdev147@gmail.com>
2016-10-15 22:58:17 +0630

    local photo upload in suggestion fragment

--- app/src/main/java/myanmarnightlife/lower/team1/fragments/SuggestFragment.java
+++ app/src/main/java/myanmarnightlife/lower/team1/fragments/SuggestFragment.java
@@ -1,15 +1,29 @@
 package myanmarnightlife.lower.team1.fragments;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import myanmarnightlife.lower.team1.MyanmarNightLifeApp;
 import myanmarnightlife.lower.team1.R;
 
@@ -19,6 +33,14 @@ import myanmarnightlife.lower.team1.R;
 public class SuggestFragment extends Fragment {
 
     String[] type = {"Beer Shop","Karaoke","Night Club","Bar","Massage","Restaurant"};
+
+    public static final int IMAGE_GALLERY_REQUEST = 20;
+
+    @BindView(R.id.btn_choose_shop_image)
+    Button btnChooseShopImage;
+
+    @BindView(R.id.img_shop)
+    ImageView imgShop;
 
     public SuggestFragment() {
         // Required empty public constructor
@@ -30,12 +52,73 @@ public class SuggestFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_suggest, container, false);
+        ButterKnife.bind(this,view);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MyanmarNightLifeApp.getContext(), android.R.layout.simple_spinner_item, type);
         MaterialBetterSpinner materialDesignSpinner = (MaterialBetterSpinner) view.findViewById(R.id.android_material_design_spinner);
         materialDesignSpinner.setAdapter(arrayAdapter);
 
+        btnChooseShopImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImageGalleryClicked(view);
+            }
+        });
+
         return view;
     }
 
+    public void openImageGalleryClicked(View view){
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        String pictureDirectoryPath = pictureDirectory.getPath();
+
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+        photoPickerIntent.setDataAndType(data, "image/*");
+
+        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+            // if we are here, everything processed successfully.
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                // if we are here, we are hearing back from the image gallery.
+
+                // the address of the image on the SD Card.
+                Uri imageUri = data.getData();
+
+                // declare a stream to read the image data from the SD Card.
+                InputStream inputStream;
+
+                // we are getting an input stream, based on the URI of the image.
+                try {
+                    inputStream = getActivity().getContentResolver().openInputStream(imageUri);
+
+                    // get a bitmap from the stream.
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+
+
+                    // show the image to the user
+                    imgShop.setImageBitmap(image);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    // show a message to the user indictating that the image is unavailable.
+                    Toast.makeText(MyanmarNightLifeApp.getContext(), "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }
+
+    }
 }
