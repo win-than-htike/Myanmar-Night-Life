f9abb9838ea919fab8f1f3e4f461a0ea6e416a3c
Ye lin aung <hello.yelinaung@gmail.com>
2016-10-29 14:04:39 +0630

    Fixed Items

--- app/src/main/java/myanmarnightlife/lower/team1/activities/DetailPagerActivity.java
+++ app/src/main/java/myanmarnightlife/lower/team1/activities/DetailPagerActivity.java
@@ -1,131 +1,107 @@
 package myanmarnightlife.lower.team1.activities;
 
-import android.annotation.TargetApi;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowInsets;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import android.view.Window;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import java.util.ArrayList;
+import java.util.List;
 import myanmarnightlife.lower.team1.MyanmarNightLifeApp;
 import myanmarnightlife.lower.team1.R;
 import myanmarnightlife.lower.team1.adapters.DetailPagerAdapter;
 import myanmarnightlife.lower.team1.data.Places;
 import myanmarnightlife.lower.team1.fragments.DetailFragment;
-import myanmarnightlife.lower.team1.fragments.FavouriteFragment;
 import myanmarnightlife.lower.team1.helper.PlacesRealmHelper;
+import org.parceler.Parcels;
 
 /**
  * Created by winthanhtike on 10/12/16.
  */
 public class DetailPagerActivity extends AppCompatActivity {
 
-    private static String PLACES = "PLACES";
+  private static String PLACES = "PLACES";
 
-    private static String IE_TYPE = "IE_TYPE";
+  private static String IE_TYPE = "IE_TYPE";
 
-    private static String TYPE;
+  private static String TYPE;
 
-//    @BindView(R.id.action_up)
-//    ImageButton upButton;
-//
-//    @BindView(R.id.up_container)
-//    FrameLayout upContainer;
+  //    @BindView(R.id.action_up)
+  //    ImageButton upButton;
+  //
+  //    @BindView(R.id.up_container)
+  //    FrameLayout upContainer;
 
-    List<Integer> fragmentMap = new ArrayList<>();
+  List<Integer> fragmentMap = new ArrayList<>();
 
-    private Places mPlaces;
+  private Places mPlaces;
 
+  public static Intent newInstance(Places places, String type) {
 
-    public static Intent newInstance(Places places, String type) {
+    Intent intent = new Intent(MyanmarNightLifeApp.getContext(), DetailPagerActivity.class);
+    Bundle bundle = new Bundle();
+    bundle.putParcelable(PLACES, Parcels.wrap(places));
+    bundle.putString(IE_TYPE, type);
+    intent.putExtras(bundle);
+    return intent;
+  }
 
-        Intent intent = new Intent(MyanmarNightLifeApp.getContext(), DetailPagerActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(PLACES, Parcels.wrap(places));
-        bundle.putString(IE_TYPE, type);
-        intent.putExtras(bundle);
-        return intent;
+  @BindView(R.id.viewPager_detail) ViewPager viewPager;
 
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.detail_activity_pager);
+    ButterKnife.bind(this, this);
+
+    if (getIntent().getExtras() != null) {
+      Bundle bundle = getIntent().getExtras();
+      mPlaces = Parcels.unwrap(bundle.getParcelable(PLACES));
+      TYPE = bundle.getString(IE_TYPE);
     }
 
-    @BindView(R.id.viewPager_detail)
-    ViewPager viewPager;
+    //        upButton.setOnClickListener(new View.OnClickListener() {
+    //            @Override
+    //            public void onClick(View view) {
+    //                onBackPressed();
+    //            }
+    //        });
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_activity_pager);
-        ButterKnife.bind(this, this);
+    DetailPagerAdapter mAdapter = new DetailPagerAdapter(getSupportFragmentManager());
 
-
-        if (getIntent().getExtras() != null) {
-            Bundle bundle = getIntent().getExtras();
-            mPlaces = Parcels.unwrap(bundle.getParcelable(PLACES));
-            TYPE = bundle.getString(IE_TYPE);
-        }
-
-
-//        upButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                onBackPressed();
-//            }
-//        });
-
-        DetailPagerAdapter mAdapter = new DetailPagerAdapter(getSupportFragmentManager());
-
-        PlacesRealmHelper helper = PlacesRealmHelper.getInstance();
-        viewPager.setAdapter(mAdapter);
-        for (Places places : helper.getBarList(TYPE)) {
-            fragmentMap.add(places.get_id());
-            mAdapter.add(DetailFragment.newInstance(places));
-        }
-
-        mAdapter.notifyDataSetChanged();
-
-        viewPager.setPageMargin((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
-
-        viewPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
-
-        viewPager.setCurrentItem(fragmentMap.indexOf(mPlaces.get_id()), false);
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
+    PlacesRealmHelper helper = PlacesRealmHelper.getInstance();
+    viewPager.setAdapter(mAdapter);
+    for (Places places : helper.getBarList(TYPE)) {
+      fragmentMap.add(places.get_id());
+      mAdapter.add(DetailFragment.newInstance(places));
     }
 
+    mAdapter.notifyDataSetChanged();
 
+    viewPager.setPageMargin((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1,
+        getResources().getDisplayMetrics()));
+
+    viewPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
+
+    viewPager.setCurrentItem(fragmentMap.indexOf(mPlaces.get_id()), false);
+    viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+      @Override
+      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+      }
+
+      @Override public void onPageSelected(int position) {
+
+      }
+
+      @Override public void onPageScrollStateChanged(int state) {
+
+      }
+    });
+  }
 }
--- app/src/main/java/myanmarnightlife/lower/team1/adapters/PlacesRVAdapter.java
+++ app/src/main/java/myanmarnightlife/lower/team1/adapters/PlacesRVAdapter.java
@@ -1,6 +1,7 @@
 package myanmarnightlife.lower.team1.adapters;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,18 +10,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-
-import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.bumptech.glide.Glide;
 import io.realm.Realm;
 import io.realm.exceptions.RealmException;
+import java.util.List;
 import myanmarnightlife.lower.team1.MyanmarNightLifeApp;
 import myanmarnightlife.lower.team1.R;
 import myanmarnightlife.lower.team1.data.Places;
+import myanmarnightlife.lower.team1.databinding.BeerCardItemBinding;
 import myanmarnightlife.lower.team1.interfaces.ItemClickListener;
 import myanmarnightlife.lower.team1.utils.Constants;
 
@@ -29,136 +28,122 @@ import myanmarnightlife.lower.team1.utils.Constants;
  */
 public class PlacesRVAdapter extends RecyclerView.Adapter<PlacesRVAdapter.PlcacesShopViewHolder> {
 
-    private LayoutInflater inflater;
-    private List<Places> mPlacesLists;
+  private LayoutInflater inflater;
+  private List<Places> mPlacesLists;
+  private ItemClickListener itemClickListener;
+  private Context context;
+
+  public PlacesRVAdapter(List<Places> mPlacesLists, ItemClickListener itemClickListener,
+      Context context) {
+    this.mPlacesLists = mPlacesLists;
+    this.itemClickListener = itemClickListener;
+    this.context = context;
+    inflater = LayoutInflater.from(MyanmarNightLifeApp.getContext());
+  }
+
+  @Override public PlcacesShopViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View rootView = inflater.inflate(R.layout.beer_card_item, parent, false);
+
+    return new PlcacesShopViewHolder(rootView, itemClickListener);
+  }
+
+  @Override public void onBindViewHolder(PlcacesShopViewHolder holder, int position) {
+
+    Places places = mPlacesLists.get(position);
+    holder.setData(places);
+
+    if (places.getIsSaved() == Constants.SAVED) {
+
+      holder.fav.setImageResource(R.drawable.ic_favorite_white_24dp);
+    } else {
+
+      holder.fav.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+    }
+  }
+
+  public Places getSelectedPlace(int position) {
+    return mPlacesLists.get(position);
+  }
+
+  @Override public int getItemCount() {
+    return mPlacesLists.size();
+  }
+
+  public class PlcacesShopViewHolder extends RecyclerView.ViewHolder
+      implements View.OnClickListener {
+    BeerCardItemBinding beerCardItemBinding;
+
+    @BindView(R.id.iv_beer_shop) ImageView imgShop;
+
+    @BindView(R.id.tv_shop_name) TextView tvShopName;
+
+    @BindView(R.id.rate_beer_shop) RatingBar shopRatingBar;
+
+    @BindView(R.id.iv_fav) ImageView fav;
+
+    private Realm realm;
+
+    private Places places;
+
     private ItemClickListener itemClickListener;
-    private Context context;
 
-    public PlacesRVAdapter(List<Places> mPlacesLists, ItemClickListener itemClickListener, Context context) {
-        this.mPlacesLists = mPlacesLists;
-        this.itemClickListener = itemClickListener;
-        this.context = context;
-        inflater = LayoutInflater.from(MyanmarNightLifeApp.getContext());
+    public PlcacesShopViewHolder(View itemView, ItemClickListener itemClickListener) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
+      beerCardItemBinding = DataBindingUtil.bind(itemView);
+      this.itemClickListener = itemClickListener;
+      itemView.setOnClickListener(this);
     }
 
-    @Override
-    public PlcacesShopViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View rootView = inflater.inflate(R.layout.beer_card_item,parent,false);
-        return new PlcacesShopViewHolder(rootView,itemClickListener);
-    }
+    public void setData(final Places places) {
 
-    @Override
-    public void onBindViewHolder(PlcacesShopViewHolder holder, int position) {
+      this.places = places;
 
-        Places places = mPlacesLists.get(position);
-        holder.setData(places);
+      shopRatingBar.setRating(Float.parseFloat(places.getRating()));
 
-        if (places.getIsSaved() == Constants.SAVED){
 
-            holder.fav.setImageResource(R.drawable.ic_favorite_white_24dp);
+      tvShopName.setText(places.getShopName());
 
-        }else {
+      fav.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
 
-            holder.fav.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+          Log.i("Name", places.get_id() + "");
+          try {
 
+            realm = Realm.getDefaultInstance();
+
+            realm.beginTransaction();
+
+            if (places.getIsSaved() == Constants.SAVED) {
+              places.setIsSaved(Constants.UNSAVED);
+              fav.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+            } else {
+              places.setIsSaved(Constants.SAVED);
+              fav.setImageResource(R.drawable.ic_favorite_white_24dp);
+            }
+
+            realm.copyToRealmOrUpdate(places);
+
+            realm.commitTransaction();
+          } catch (RealmException e) {
+            e.printStackTrace();
+          }
+
+          notifyDataSetChanged();
         }
+      });
 
+      Glide.with(MyanmarNightLifeApp.getContext())
+          .load(places.getShopImage())
+          .centerCrop()
+          .crossFade()
+          .error(R.drawable.night)
+          .placeholder(R.drawable.night)
+          .into((imgShop));
     }
 
-    public Places getSelectedPlace(int position){
-        return mPlacesLists.get(position);
+    @Override public void onClick(View view) {
+      if (itemClickListener != null) itemClickListener.onTapShop(places, imgShop);
     }
-
-    @Override
-    public int getItemCount() {
-        return mPlacesLists.size();
-    }
-
-    public class PlcacesShopViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-        @BindView(R.id.iv_beer_shop)
-        ImageView imgShop;
-
-        @BindView(R.id.tv_shop_name)
-        TextView tvShopName;
-
-        @BindView(R.id.rate_beer_shop)
-        RatingBar shopRatingBar;
-
-        @BindView(R.id.iv_fav)
-        ImageView fav;
-
-        private Realm realm;
-
-        private Places places;
-
-        private ItemClickListener itemClickListener;
-
-        public PlcacesShopViewHolder(View itemView, ItemClickListener itemClickListener) {
-            super(itemView);
-            ButterKnife.bind(this,itemView);
-            this.itemClickListener = itemClickListener;
-            itemView.setOnClickListener(this);
-
-        }
-
-        public void setData(final Places places){
-
-            this.places = places;
-
-            shopRatingBar.setRating(Float.parseFloat(places.getRating()));
-
-            tvShopName.setText(places.getShopName());
-
-
-            fav.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    Log.i("Name",places.get_id()+"");
-                    try {
-
-                        realm = Realm.getDefaultInstance();
-
-                        realm.beginTransaction();
-
-                        if(places.getIsSaved() == Constants.SAVED) {
-                            places.setIsSaved(Constants.UNSAVED);
-                            fav.setImageResource(R.drawable.ic_favorite_border_white_24dp);
-                        }
-                        else {
-                            places.setIsSaved(Constants.SAVED);
-                            fav.setImageResource(R.drawable.ic_favorite_white_24dp);
-                        }
-
-                        realm.copyToRealmOrUpdate(places);
-
-
-                        realm.commitTransaction();
-
-
-                    }catch (RealmException e){
-                        e.printStackTrace();
-                    }
-
-                    notifyDataSetChanged();
-                }
-            });
-
-
-
-            Glide.with(MyanmarNightLifeApp.getContext())
-                    .load(places.getShopImage())
-                    .centerCrop()
-                    .crossFade()
-                    .error(R.mipmap.ic_launcher)
-                    .placeholder(R.drawable.night)
-                    .into((imgShop));
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (itemClickListener != null) itemClickListener.onTapShop(places,imgShop);
-        }
-    }
+  }
 }
