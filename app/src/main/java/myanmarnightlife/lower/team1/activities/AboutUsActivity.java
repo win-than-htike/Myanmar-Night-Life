59d7ae70d31bc672508c9d0e6a39772707d98a74
Ye lin aung <hello.yelinaung@gmail.com>
2016-11-02 02:00:02 +0630

    Fixed Fonts In About

--- app/src/main/java/myanmarnightlife/lower/team1/activities/AboutUsActivity.java
+++ app/src/main/java/myanmarnightlife/lower/team1/activities/AboutUsActivity.java
@@ -4,106 +4,95 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import myanmarnightlife.lower.team1.MyanmarNightLifeApp;
 import myanmarnightlife.lower.team1.R;
+import myanmarnightlife.lower.team1.databinding.ActivityAboutUsBinding;
 
 public class AboutUsActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn_winthanhtike)
-    Button btnWinthan;
+  @BindView(R.id.btn_winthanhtike) Button btnWinthan;
 
-    @BindView(R.id.btn_aungkhantwai)
-    Button btnAungKhantWai;
+  @BindView(R.id.btn_aungkhantwai) Button btnAungKhantWai;
 
-    @BindView(R.id.btn_aungmyothu)
-    Button btnAungMyoThu;
+  @BindView(R.id.btn_aungmyothu) Button btnAungMyoThu;
 
-    @BindView(R.id.btn_facebook)
-    Button btnFacebook;
+  @BindView(R.id.btn_facebook) Button btnFacebook;
 
-    @BindView(R.id.btn_googleplus)
-    Button btnGooglePlus;
+  @BindView(R.id.btn_googleplus) Button btnGooglePlus;
 
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    ActivityAboutUsBinding activityAboutUsBinding =
+        DataBindingUtil.setContentView(this, R.layout.activity_about_us);
+    ButterKnife.bind(this, this);
 
+    btnWinthan.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        sendEmail("winthanhtike@ucsy.edu.mm", "Dear Win Than Htike,");
+      }
+    });
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ButterKnife.bind(this,this);
+    btnAungKhantWai.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        sendEmail("johnaungkhant@gmail.com", "Dear Aung Khant Wai,");
+      }
+    });
 
-        btnWinthan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendEmail("winthanhtike@ucsy.edu.mm","Dear Win Than Htike,");
-            }
-        });
+    btnAungMyoThu.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        sendEmail("tunlin009707@gmail.com", "Dear Aung Myo Thu,");
+      }
+    });
 
-        btnAungKhantWai.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendEmail("johnaungkhant@gmail.com","Dear Aung Khant Wai,");
-            }
-        });
+    btnFacebook.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        startActivity(getOpenFacebookIntent(MyanmarNightLifeApp.getContext()));
+      }
+    });
 
-        btnAungMyoThu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendEmail("tunlin009707@gmail.com","Dear Aung Myo Thu,");
-            }
-        });
+    btnGooglePlus.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        startActivity(getOpenGooglePlusIntent(MyanmarNightLifeApp.getContext()));
+      }
+    });
+  }
 
-        btnFacebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(getOpenFacebookIntent(MyanmarNightLifeApp.getContext()));
-            }
-        });
+  public static Intent getOpenGooglePlusIntent(Context context) {
 
-        btnGooglePlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(getOpenGooglePlusIntent(MyanmarNightLifeApp.getContext()));
-            }
-        });
-
+    try {
+      context.getPackageManager().getPackageInfo("com.google.android.apps.plus", 0);
+      Intent googlePlus = new Intent(Intent.ACTION_VIEW,
+          Uri.parse("https://plus.google.com/u/2/118109565446882060041"));
+      googlePlus.putExtra("customAppUri", "118109565446882060041");
+      return googlePlus;
+    } catch (Exception e) {
+      return new Intent(Intent.ACTION_VIEW,
+          Uri.parse("https://plus.google.com/u/2/118109565446882060041"));
     }
+  }
 
-    public static Intent getOpenGooglePlusIntent(Context context) {
+  public static Intent getOpenFacebookIntent(Context context) {
 
-        try {
-            context.getPackageManager().getPackageInfo("com.google.android.apps.plus", 0);
-            Intent googlePlus = new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/u/2/118109565446882060041"));
-            googlePlus.putExtra("customAppUri","118109565446882060041");
-            return googlePlus;
-        } catch (Exception e) {
-            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/u/2/118109565446882060041"));
-        }
+    try {
+      context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+      return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/1839413723005995"));
+    } catch (Exception e) {
+      return new Intent(Intent.ACTION_VIEW,
+          Uri.parse("https://www.facebook.com/myanmarnightlifewaa/"));
     }
+  }
 
-    public static Intent getOpenFacebookIntent(Context context) {
-
-        try {
-            context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
-            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/1839413723005995"));
-        } catch (Exception e) {
-            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/myanmarnightlifewaa/"));
-        }
-    }
-
-    public void sendEmail(String email,String header){
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_EMAIL  , new String[]{email});
-        shareIntent.putExtra(Intent.EXTRA_TEXT, header);
-        shareIntent.setType("text/plain");
-        startActivity(Intent.createChooser(shareIntent, "Send To..."));
-    }
-
+  public void sendEmail(String email, String header) {
+    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+    shareIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { email });
+    shareIntent.putExtra(Intent.EXTRA_TEXT, header);
+    shareIntent.setType("text/plain");
+    startActivity(Intent.createChooser(shareIntent, "Send To..."));
+  }
 }
